c47b3fd84ba7387d42cb9774551e840e411f8a09
ruthnadiezhda <a20150875@pucp.edu.pe>
2020-07-21 12:00:19 -0500

    Iniciando 1

--- index.js
+++ index.js
@@ -23,3 +23,21 @@ app.get('/login',function (request, response) {
     console.log("Nuevo log");
     response.sendFile(__dirname + '/login.html');
 });
+
+//Conexion con la BD
+const express = require('express');
+const mysql = require('mysql2');
+
+const app = express();
+
+let conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'exfinal'
+});
+
+conn.connect(function(err){
+    if(err) throw err;
+    console.log("Conexión exitosa a base de datos");
+});
